1d24c3ff095a3ea09102d33e80267db3c6e3a64c
Allan <allancarlosdeveloper@gmail.com>
2018-04-03 20:51:54 -0300

    actions, reducers and store

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import { createStore } from 'redux';
+import reducer from './reducers';
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
