99f226640216b56493d299ec14fa3d5fee1007ea
Allan <allancarlosdeveloper@gmail.com>
2018-04-02 21:52:04 -0300

    App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        Hello World
-      </div>
-    )
+    return <div>Hello World</div>;
   }
 }
 
-export default App
+export default App;
